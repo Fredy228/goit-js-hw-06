d6847ef0c3e0ba5530e1ec4f6f979c27fd021007
Alexei Smahrovych <smagrovich58@gmail.com>
2022-10-06 13:58:18 +0300

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,36 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+const refss = {
+  btnCreate: document.querySelector('[data-create]'),
+  btnDetroy: document.querySelector('[data-destroy]'),
+  valueInput: document.querySelector('#controls').firstElementChild,
+  divBoxes: document.querySelector('#boxes'),
+};
+
+function createBoxes(amount) {
+  const arrayBoxes = [];
+  for (let i = 30; i < amount*10+30; i += 10) {
+     function createBox() {
+      const box = `<div style="width: ${i}px; height: ${i}px; background-color: ${getRandomHexColor()}"></div>`;
+      return box;
+    }
+    arrayBoxes.push(createBox())
+  };
+
+  refss.divBoxes.insertAdjacentHTML("afterbegin", arrayBoxes.join(""));
+};
+
+function destroyBoxes() {
+  while (refss.divBoxes.firstChild) {
+    refss.divBoxes.removeChild(refss.divBoxes.firstChild);
+  };
+  refss.valueInput.value = "";
+};
+
+refss.btnCreate.addEventListener("click", () => {
+  createBoxes(refss.valueInput.value);
+})
+
+refss.btnDetroy.addEventListener("click", destroyBoxes);
