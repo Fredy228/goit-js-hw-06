8e80945327f9bed0db95ba25a09679014a3a45d9
Alexei Smahrovych <smagrovich58@gmail.com>
2022-10-06 10:59:42 +0300

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  valueColor: document.querySelector('.color'),
+  btnChangeColor: document.querySelector('.change-color'),
+};
+
+refs.btnChangeColor.addEventListener("click", () => {
+  document.body.style.backgroundColor=getRandomHexColor();
+  refs.valueColor.textContent = getRandomHexColor();
+});
