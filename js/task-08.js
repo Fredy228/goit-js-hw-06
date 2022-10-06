6772b5b71a1ab9c0e6d34b109df232c45da389df
Alexei Smahrovych <smagrovich58@gmail.com>
2022-10-06 10:48:09 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+      } = event.currentTarget;
+    
+      if (email.value === "" || password.value === "") {
+        window.alert("Bсе поля должны быть заполнены");
+      } else {
+        console.log({
+            email: email.value,
+            password: password.value,
+        })
+        form.reset();
+      }
+});
 
